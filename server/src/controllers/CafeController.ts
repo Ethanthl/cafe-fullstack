b55807426ab105e974ba4869406b3bfccbd388ae
Ethan <info@ethanthl.com>
2023-08-22 16:57:52 +1000

    replace ID with uuid generator

--- server/src/controllers/CafeController.ts
+++ server/src/controllers/CafeController.ts
@@ -2,6 +2,7 @@ import Express, { RequestHandler } from "express";
 import { StatusCodes } from "http-status-codes";
 import { pool } from "../../src/config/database";
 import Joi from "joi";
+import { v4 as uuidv4 } from "uuid";
 
 const CafeController = Express.Router();
 
@@ -20,27 +21,41 @@ const cafeSchema = Joi.object({
   logo: Joi.string().custom(base64Validator).allow(""),
 });
 
-const cafeGetHandler: RequestHandler = async (req, res) => {
-  const connection = await pool.getConnection();
-  const location = "%" + req.query.location + "%";
-  // switch queries based on location param provided
-  const selectQuery = req.query.location
-    ? `SELECT c.*, COUNT(e.id) AS employees FROM cafe c LEFT JOIN employee e ON c.id = e.cafe_id
-    WHERE location LIKE ?  GROUP BY c.id
-    ORDER BY employees DESC;`
-    : `SELECT c.*, COUNT(e.id) AS employees
-    FROM cafe c
-    LEFT JOIN employee e ON c.id = e.cafe_id
-    GROUP BY c.id
-    ORDER BY employees DESC;`;
-
-  const [rows]: any = await connection.execute(selectQuery, [location]);
-
-  console.log(rows);
-
-  return res.sendStatus(StatusCodes.OK);
+//Generate UUID for entries
+const generateUUID = () => {
+  const uuid = uuidv4().replace(/-/g, "").substring(0, 8);
+  return `UI${uuid}`;
 };
 
+//Get cafes based on location or all if none
+//Ordered DESC based on employee count
+const cafeGetHandler: RequestHandler = async (req, res) => {
+  const connection = await pool.getConnection();
+  try {
+    const location = "%" + req.query.location + "%";
+    // switch queries based on location param provided
+    const selectQuery = req.query.location
+      ? `SELECT c.*, COUNT(e.id) AS employees FROM cafe c LEFT JOIN employee e ON c.id = e.cafe_id
+      WHERE location LIKE ?  GROUP BY c.id
+      ORDER BY employees DESC;`
+      : `SELECT c.*, COUNT(e.id) AS employees
+      FROM cafe c
+      LEFT JOIN employee e ON c.id = e.cafe_id
+      GROUP BY c.id
+      ORDER BY employees DESC;`;
+
+    const [rows]: any = await connection.execute(selectQuery, [location]);
+
+    return res.status(StatusCodes.OK).json({ cafes: rows });
+  } catch (error) {
+    console.log(error);
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+  } finally {
+    connection.release();
+  }
+};
+
+//Add cafe to database
 const cafePostHandler: RequestHandler = async (req, res) => {
   const connection = await pool.getConnection();
   try {
@@ -54,13 +69,8 @@ const cafePostHandler: RequestHandler = async (req, res) => {
 
     //Since mySQL does not support sequence, I've stored the number in a table
     //Get the current counter from the database and create UUID
-    const [rows]: any = await connection.execute(
-      "SELECT counter FROM counter_table WHERE id = 1"
-    );
-    const currentCounter = rows[0].counter;
-    const UUID = "UI" + currentCounter;
 
-    console.log(UUID);
+    const UUID = generateUUID();
 
     const insertQuery = `INSERT INTO cafe (id, name, description, logo, location) VALUES (?, ?, ?, ?, ?)`;
 
@@ -74,21 +84,13 @@ const cafePostHandler: RequestHandler = async (req, res) => {
     ]);
 
     if (result.affectedRows > 0) {
-      console.log("Insertion successful.");
-
-      // Update the counter value in the database
-      const [update]: any = await connection.execute(
-        "UPDATE counter_table SET counter = counter + 1 WHERE id = 1"
-      );
-
-      console.log("Counter updated.");
-      res.status(StatusCodes.OK).send("Cafe added successfully.");
+      return res.status(StatusCodes.OK).send("Cafe added successfully.");
     } else {
       throw new Error("Database error");
     }
   } catch (error) {
     console.log(error);
-    res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
   } finally {
     connection.release();
   }
